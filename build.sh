6d4c32c92dbb1c24a2d6713e8ba842dc9b1c62d7
Christopher Bradley <chris.bradley@cy.id.au>
2017-06-17 10:05:58 +0000

    Convert project to new format, remove zip install from build shell script, update dependency versions, add google api dependencies

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+# Fail if any command fails
+set -e
 
 #build handlers
 dotnet restore
 dotnet publish -c release
-
-#install zip
-apt-get -qq update
-apt-get -qq -y install zip
 
 #create deployment package
 pushd bin/release/netcoreapp1.0/publish
